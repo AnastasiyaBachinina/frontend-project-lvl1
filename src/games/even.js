e58b3bd7bd42077c27749cf3c197ef0e09cab3e4
Sergey Bachinin <bachinin_sergey@mail.ru>
2022-06-02 21:53:08 +0300

    Mooved common logic from even.js to index.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,41 +1,17 @@
-import readlineSync from 'readline-sync';
-
-const roundsCount = 3;
-
-const engine = (gameRules, gameCheck) => {
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-  console.log(gameRules);
-
-  for (let i = 1; i <= roundsCount; i += 1) {
-    const [getQuestion, correctAnswer] = gameCheck();
-    console.log(`Question: ${getQuestion}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-
-    if (userAnswer !== correctAnswer) {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${userName}!`);
-      return;
-    }
-    console.log('Correct!');
-  }
-  console.log(`Congratulations, ${userName}!`);
-};
-
-const getRandomNumber = (min, max) => Math.floor(min + Math.random() * (max + 1 - min));
+import gameEngine from '../index.js';
+import getRandomNumber from '../getRandomNumber.js';
 
 const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const isEven = (number) => number % 2 === 0;
 
-const gameCheck = () => {
+const makeRound = () => {
   const number = getRandomNumber(1, 100);
   const question = `${number}`;
   const correctAnswer = isEven(number) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
 
-const evenGame = () => engine(gameRules, gameCheck);
+const evenGame = () => gameEngine(gameRules, makeRound);
 
 export default evenGame;
